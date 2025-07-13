b4f33d7ed2c83a006aab3fd8bc740afa4b4faa4a
drb101005 <drb101005@gmail.com>
2025-07-13 12:26:25 +0530

    Initialize React app with Create React App
    Set up a new React project using Create React App. Added initial project structure, configuration files, and a sample component in src/Mycomps/header.js.

---
+++ src/Mycomps/header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Header = ({ subname }) => {
+  return (
+    <div>
+    <h1>Hello.. {subname}</h1>
+    <h2>whyy</h2>
+    </div>
+  );
+};
+
+export default Header;
+
